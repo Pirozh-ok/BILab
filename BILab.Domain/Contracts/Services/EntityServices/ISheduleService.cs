95edce47f625e2e5968dc7f755efe6e9f0776bde
Иван Воротников <ivan-vorotnikov@outlook.com>
2024-05-21 23:25:31 +0700

    updated IScheduleService

--- BILab.Domain/Contracts/Services/EntityServices/ISheduleService.cs
+++ BILab.Domain/Contracts/Services/EntityServices/ISheduleService.cs
@@ -5,11 +5,7 @@ using BILab.Domain.Models.Entities;
 
 namespace BILab.Domain.Contracts.Services.EntityServices {
     public interface ISheduleService : ISearchableEntityService<Shedule, Guid, SheduleDTO, PageableSheduleRequestDto> {
-<<<<<<< HEAD
         Task<ServiceResult> GetFreeShedule(Guid employeeId, DateTime checkData);
-=======
-        Task<ServiceResult> GetFreeShedule(Guid employeeId, DayOfWeek day);
->>>>>>> bb1ec733aa37ddf2b78cdc966910aaabf16a3964
         Task<ServiceResult> GetScheduleByEmployee(Guid employeeId);
     }
 }
